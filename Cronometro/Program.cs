4f6bd25bb8c87380c604ce1b3e09a77144ec7b12
Balu <balu@email.com>
2023-11-03 17:00:23 -0300

    Adição do projeto Cronometro

---
+++ Cronometro/Program.cs
@@ -0,0 +1,39 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.WriteLine("\n!!CRONOMETRO!!\n");
+    Console.WriteLine("\nEscolha uma opção:\n(S) para Segundos\n(M) para Minutos\n(0) para Sair.");
+    string valor = Console.ReadLine();
+    char tipo = char.Parse(valor.Substring(valor.Length-1,1).ToLower());
+    int tempo = int.Parse(valor.Substring(0,valor.Length - 1));
+    app(tempo, tipo);
+
+
+}
+
+static void app(int tempo, char tipo)
+{
+    int mult = 1;
+    int contagem = 0;
+
+    if(tipo == 'm')
+    {
+        mult = 60;
+        tempo *= mult;
+    }
+    if (tempo == 0)
+    {
+        System.Environment.Exit(0);
+    }
+
+    while (contagem != tempo)
+    {
+        Console.Clear();
+        contagem++;
+        Console.WriteLine(contagem);
+        Thread.Sleep(1000);
+    }
+    
+}
+
